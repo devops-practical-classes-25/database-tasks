93d38c3544f6c9a97c36ae3217b22ae5401c55f3
Uliana Degtiareva <uvdegtiareva@edu.hse.ru>
2025-03-23 23:52:45 +0500

    правки для инициализации бд

--- init/init-db.sh
+++ init/init-db.sh
@@ -3,12 +3,20 @@
 
 set -e
 
-echo run init script
+echo "Waiting for PostgreSQL to start..."
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER $DB_USER WITH LOGIN PASSWORD '$DB_PASSWORD';
-	CREATE DATABASE $DB_NAME OWNER $DB_USER;
+# Ожидание, пока база данных не будет готова
+until pg_isready -h postgres -p 5432 -U "$POSTGRES_USER"; do
+  echo "Waiting for database..."
+  sleep 2
+done
+
+echo "run init script"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --host "postgres" --port 5432 --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER $DB_USER WITH LOGIN PASSWORD '$DB_PASSWORD';
+  CREATE DATABASE $DB_NAME OWNER $DB_USER;
 EOSQL
 
-echo run sql script
-psql -v ON_ERROR_STOP=1 --username "$DB_USER" --dbname "$DB_NAME" -f /docker-entrypoint-initdb.d/sql/init.sql
+echo "run sql script"
+psql -v ON_ERROR_STOP=1 --username "$DB_USER" --host "postgres" --port 5432 --dbname "$DB_NAME" -f /docker-entrypoint-initdb.d/sql/init.sql
